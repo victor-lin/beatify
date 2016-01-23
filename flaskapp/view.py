ceafddcb7f091fc065c30dccc635e80ec7b690d5
Victor Lin <vlin@acceleration.net>
2016-01-23 16:02:50 -0500

    Add Flask view, basic layout

---
+++ flaskapp/view.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+app.debug = True
+
+
+@app.route('/')
+def hello_world():
+    return render_template('start.html')
+
+if __name__ == '__main__':
+    app.run()
